e28fc0395e4fe5316b9515cf9b1e2cc1227a2d42
Benjamin Nicovani <b.nicovani@duocuc.cl>
2026-05-17 22:20:07 -0400

    Actualizar Procfile y settings.py para usar dj-database-url y agregar psycopg2-binary a requirements.txt

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput && gunicorn lbwus.wsgi
+web: python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn lbwus.wsgi --bind 0.0.0.0:$PORT
--- lbwus/settings.py
+++ lbwus/settings.py
@@ -79,11 +79,16 @@ WSGI_APPLICATION = 'lbwus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
